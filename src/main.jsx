d88df84836624727659782b7c2deb7f8be9040d2
Shoham Shtiler <shtilershoham@gmail.com>
2025-05-09 14:52:22 +0300

    Init scss folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './RootCmp.jsx'
 import './assets/style/base/base.css'
+import './assets/style/scss/style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
